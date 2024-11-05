6694fcff7d4927a53cb85a46d189f80ed644ebe0
Shade78 <shademoon322@gmail.com>
2024-11-05 23:16:48 +0300

    added delete article fucntionality
    and slugify for url based on title

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const articleRouter = require("./routes/articles");
 const mongoose = require("mongoose");
 const Article = require("./models/article"); // getting article model
+const methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost/markdownBlog");
 
@@ -10,6 +11,8 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
+
+app.use(methodOverride("_method"));
 
 app.get("/", async (req, res) => {
   const articles = await Article.find().sort({ createdAt: "desc" });
