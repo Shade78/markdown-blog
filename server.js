01c3c5ae3e4ca2b36d9c7c7d47d10d54fe4244cd
Shade78 <shademoon322@gmail.com>
2024-11-05 04:42:05 +0300

    saving articles to db and render all articles in main page
    also redirecting to new article page after create one

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 const express = require("express");
 const articleRouter = require("./routes/articles");
+const mongoose = require("mongoose");
+const Article = require("./models/article"); // getting article model
+
+mongoose.connect("mongodb://localhost/markdownBlog");
 
 const app = express();
 
 app.set("view engine", "ejs");
 
-app.use("/articles", articleRouter);
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Test Article",
-      createdAt: new Date(),
-      description: "Test description",
-    },
-    {
-      title: "Test Article 2",
-      createdAt: new Date(),
-      description: "Test description 2",
-    },
-  ];
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
   res.render("articles/index", { articles: articles });
 });
+
+app.use("/articles", articleRouter);
 
 app.listen(5000);
